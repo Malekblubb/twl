48b2e24586543d4e722803ce44f5160c1862ad21
Christoph Malek <malekchristoph@gmail.com>
2013-10-30 19:59:23 +0100

    twl::map_utl::internal::make_group impl

--- include/twl/files/map_extended/map_utl.h
+++ include/twl/files/map_extended/map_utl.h
@@ -9,6 +9,8 @@
 
 #include "items/group.h"
 
+#include <twl/files/map_datafile/map_datafile_items.h>
+
 
 namespace twl
 {
@@ -16,9 +18,18 @@ namespace twl
 	{
 		namespace internal
 		{
-			map_itm::map_group make_group()
+			// helper function to make a extended group out of a datafile_group
+			// i don't want this as constructor in map_itm::map_group
+			map_itm::map_group make_group(const file::internal::map_datafile_group& group)
 			{
-
+				map_itm::map_group result;
+				result.set_clipping(group.clipping());
+				result.set_clip_h(group.clip_h()); result.set_clip_w(group.clip_w());
+				result.set_clip_x(group.clip_x()); result.set_clip_y(group.clip_y());
+				result.set_order(-1);
+				result.set_para_x(group.para_x()); result.set_para_y(group.para_y());
+				result.set_pos_x(group.offset_x()); result.set_pos_y(group.offset_y()); // TODO: check this
+				return result;
 			}
 		}
 	}
