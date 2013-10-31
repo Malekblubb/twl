ca8d5abd8d77572b853d66b33eefb332d29c12db
Christoph Malek <malekchristoph@gmail.com>
2013-10-31 19:59:01 +0100

    better layer structure?

--- include/twl/files/map_extended/items/layer.h
+++ include/twl/files/map_extended/items/layer.h
@@ -9,39 +9,66 @@
 
 #include "item_base.h"
 
+#include <twl/files/map_datafile/map_datafile_types.h>
+
+#include <mlk/tools/enum_utl.h>
+
 
 namespace twl
 {
 	namespace map_itm
 	{
+		using layer_type = file::layer_type;
+
 		namespace internal
 		{
 			class map_layer_base
 			{
+				int m_type{0};
+				int m_flags{0};
 
 			public:
+				map_layer_base(int type, int flags) :
+					m_type{type},
+					m_flags{flags}
+				{ }
 
 			};
 		}
 
 
-		class map_tiles_layer :
-				public internal::map_layer_base,
-				public internal::map_item_base
+		template<layer_type type>
+		class map_layer{public: map_layer() = delete;};
+
+
+		template<>
+		class map_layer<layer_type::tiles> : public internal::map_layer_base
 		{
 
 		public:
-
+			map_layer(int flags = 0) :
+				internal::map_layer_base{mlk::enum_utl::to_int(layer_type::tiles), flags}
+			{ }
 		};
 
-		class map_quads_layer :
-				public internal::map_layer_base,
-				public internal::map_item_base
+
+		template<>
+		class map_layer<layer_type::quads> : public internal::map_layer_base
 		{
 
 		public:
-
+			map_layer(int flags = 0) :
+				internal::map_layer_base{mlk::enum_utl::to_int(layer_type::quads), flags}
+			{ }
 		};
+
+
+
+//		template<typename T, typename E>
+//		E layer_cast(const layer)
+//		{
+////			return
+//		}
 	}
 }
 
