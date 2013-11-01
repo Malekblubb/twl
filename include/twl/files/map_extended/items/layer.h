c1e724e85d15ff2c3ab440b0f98cecffaffb9d5a
Christoph Malek <malekchristoph@gmail.com>
2013-11-01 11:56:41 +0100

    layer tiles

--- include/twl/files/map_extended/items/layer.h
+++ include/twl/files/map_extended/items/layer.h
@@ -8,6 +8,7 @@
 
 
 #include "item_base.h"
+#include "image.h"
 
 #include <twl/files/map_datafile/map_datafile_types.h>
 
@@ -24,6 +25,7 @@ namespace twl
 		{
 			class map_layer_base
 			{
+			protected:
 				int m_type{0};
 				int m_flags{0};
 
@@ -33,6 +35,7 @@ namespace twl
 					m_flags{flags}
 				{ }
 
+				int type() const noexcept {return m_type;} // TODO: layer_type ?
 			};
 		}
 
@@ -44,11 +47,35 @@ namespace twl
 		template<>
 		class map_layer<layer_type::tiles> : public internal::map_layer_base
 		{
+			int m_width{100}, m_height{100};
+			int m_group{0}, m_order{0};
+			bool m_detail{false}, m_is_game_layer{false};
+			map_image m_image;
+			// TODO: color
+			std::string m_name{"Tiles"};
 
 		public:
-			map_layer(int flags = 0) :
-				internal::map_layer_base{mlk::enum_utl::to_int(layer_type::tiles), flags}
+			map_layer() :
+				internal::map_layer_base{mlk::enum_utl::to_int(layer_type::tiles), 0}
 			{ }
+
+			void set_width(int width) noexcept {m_width = width;}
+			void set_height(int height) noexcept {m_height = height;}
+			void set_group_index(int group) noexcept {m_group = group;}
+			void set_order(int order) noexcept {m_order = order;}
+			void set_detail(bool is_detail) noexcept {m_detail = is_detail;}
+			void set_as_game(bool true_false) noexcept {m_is_game_layer = true_false; m_flags |= true_false;}
+			void set_image(map_image image) noexcept {m_image = std::move(image);} // TODO: test performace without move
+			void set_name(std::string name) noexcept {m_name = std::move(name);}
+
+			int width() const noexcept {return m_width;}
+			int height() const noexcept {return m_height;}
+			int group_index() const noexcept {return m_group;}
+			int order() const noexcept {return m_order;}
+			bool is_detail() const noexcept {return m_detail;}
+			bool is_game_layer() const noexcept {return m_is_game_layer;}
+			const map_image& image() const noexcept {return m_image;}
+			std::string name() const noexcept {return m_name;}
 		};
 
 
@@ -61,14 +88,6 @@ namespace twl
 				internal::map_layer_base{mlk::enum_utl::to_int(layer_type::quads), flags}
 			{ }
 		};
-
-
-
-//		template<typename T, typename E>
-//		E layer_cast(const layer)
-//		{
-////			return
-//		}
 	}
 }
 
