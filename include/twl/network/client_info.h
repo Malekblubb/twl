7c04f510dcbbcb56756a48b2e8dc1faff7ad8286
Christoph Malek <malekchristoph@gmail.com>
2013-11-29 21:28:09 +0100

    added client_info

---
+++ include/twl/network/client_info.h
@@ -0,0 +1,32 @@
+//
+// Copyright (c) 2013 Christoph Malek
+// See LICENSE for more information.
+//
+
+#ifndef TWL_NETWORK_BASIC_CLIENT_INFO_H
+#define TWL_NETWORK_BASIC_CLIENT_INFO_H
+
+
+#include <string>
+
+
+namespace twl
+{
+
+	struct client_info
+	{
+		std::string name;
+		std::string clan;
+		int country;
+		int score;
+		int is_player;
+
+
+
+
+	};
+}
+
+
+
+#endif // TWL_NETWORK_BASIC_CLIENT_INFO_H
