86c3607ab3a936fe489f34690605253ff86efe4e
Christoph Malek <malekchristoph@gmail.com>
2014-03-15 22:23:47 +0100

    includes

---
+++ include/twl/network/network.hpp
@@ -0,0 +1,16 @@
+//
+// Copyright (c) 2013-2014 Christoph Malek
+// See LICENSE for more information.
+//
+
+#ifndef TWL_NETWORK_NETWORK_HPP
+#define TWL_NETWORK_NETWORK_HPP
+
+
+#include "player_info.hpp"
+#include "server_game.hpp"
+#include "server_info.hpp"
+#include "server_master.hpp"
+
+
+#endif // TWL_NETWORK_NETWORK_HPP
