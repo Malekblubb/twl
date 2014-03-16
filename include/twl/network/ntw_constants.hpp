06a9b49a9378781ec24252baa833a2723b404eef
Christoph Malek <malekchristoph@gmail.com>
2014-03-16 19:08:51 +0100

    ntw_constants: added ip4_spacer()

--- include/twl/network/ntw_constants.hpp
+++ include/twl/network/ntw_constants.hpp
@@ -7,15 +7,22 @@
 #define TWL_NETWORK_NTW_CONSTANTS_HPP
 
 
+#include <mlk/network/address.h>
+#include <mlk/types/types.h>
+
 #include <cstddef>
 
 
 namespace twl
 {
+	// twl
+	using masterlist = std::vector<mlk::ntw::ip_address>;
+
+	// internal
 	namespace internal
 	{
 		namespace ntw_constants
-		{
+		{			
 			inline constexpr long long default_timeout()
 			{return 999;}
 
@@ -27,6 +34,9 @@ namespace twl
 
 			inline constexpr std::size_t interaction_datasize()
 			{return 8;}
+
+			inline mlk::data_packet ip4_spacer()
+			{return {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0xff};}
 		}
 	}
 }
