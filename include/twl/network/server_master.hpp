2989c849afcbcd719f6d456500572acf3a869ced
Christoph Malek <malekchristoph@gmail.com>
2014-03-17 20:46:29 +0100

    master_server: some implementation

--- include/twl/network/server_master.hpp
+++ include/twl/network/server_master.hpp
@@ -17,13 +17,17 @@ namespace twl
 {
 	class master_server : public internal::server_base<internal::server_master_connection, internal::ntw_constants::default_timeout()>
 	{
-
 		std::vector<mlk::ntw::ip_address> m_servers;
 
+		masterlist m_listresult;
+		int m_countresult{0};
+
 	public:
+		mlk::slot<int> on_count;
+		mlk::slot<const masterlist&> on_list;
+
 		master_server()
 		{this->init();}
-
 
 		void add_master(const mlk::ntw::ip_address& addr)
 		{
@@ -34,23 +38,43 @@ namespace twl
 
 		void request_count()
 		{
+			m_countresult = 0;
 			for(auto& a : m_servers)
 				this->request<internal::server_request::master_get_count>(a);
 		}
 
 		void request_list()
 		{
-
+			m_listresult.clear();
+			for(auto& a : m_servers)
+				this->request<internal::server_request::master_get_list>(a);
 		}
+
+		int get_count() const noexcept
+		{return m_countresult;}
+
+		const auto& get_list() const noexcept
+		{return m_listresult;}
 
 	private:
 		void init()
 		{
 			this->on_recved =
-			[](const auto& data, const auto& addr)
+			[this](const auto& data, const auto& addr)
 			{
 				internal::master_parser mp{data};
-				std::cout << "recved: " << data.data() << std::endl;
+				if(mp.countreply())
+				{
+					auto count(mp.get_countresult());
+					m_countresult += count;
+					this->on_count(count);
+				}
+				else if(mp.listreply())
+				{
+					auto list(mp.get_listresult());
+					m_listresult += list;
+					this->on_list(list);
+				}
 			};
 		}
 	};
