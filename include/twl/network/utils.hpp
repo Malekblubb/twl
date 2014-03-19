baaeb538c70083d4ff7e6f9dea365ecf9fbd365b
Christoph Malek <malekchristoph@gmail.com>
2014-03-19 17:04:46 +0100

    added missing inline(s)

--- include/twl/network/utils.hpp
+++ include/twl/network/utils.hpp
@@ -15,20 +15,20 @@ namespace twl
 {
 	namespace internal
 	{
-		mlk::data_packet make_connless(const mlk::data_packet& data)
+		inline mlk::data_packet make_connless(const mlk::data_packet& data)
 		{
 			auto cpy(data);
 			mlk::cnt::insert_front(mlk::data_packet{0xff, 0xff, 0xff, 0xff, 0xff, 0xff}, cpy);
 			return cpy;
 		}
 
-		mlk::data_packet add_token(unsigned char token, mlk::data_packet& data)
+		inline mlk::data_packet add_token(unsigned char token, mlk::data_packet& data)
 		{
 			data.emplace_back(token);
 			return data;
 		}
 
-		std::string from_binary_ip(bool ip4, const mlk::data_packet& data)
+		inline std::string from_binary_ip(bool ip4, const mlk::data_packet& data)
 		{
 			auto buflen(ip4 ? INET_ADDRSTRLEN : INET6_ADDRSTRLEN);
 			std::vector<char> buf(buflen);
