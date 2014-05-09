ea74a9ff46c4a97722c4f1a4782d13dd039458f6
Christoph Malek <malekchristoph@gmail.com>
2014-05-09 20:49:30 +0200

    wip: map

--- include/twl/files/map/map.hpp
+++ include/twl/files/map/map.hpp
@@ -7,30 +7,30 @@
 #define TWL_FILES_MAP_MAP_H
 
 
-#include "map_datafile.hpp"
+#include "map_datafile_manager.hpp"
 
 
 namespace twl
 {
 	class map
 	{
-		map_datafile m_datafile;
+		internal::map_datafile_manager m_datafilemanager;
 
 	public:
 		map() = default;
 
 		map(const std::string& path) :
-			m_datafile{path}
+			m_datafilemanager{path}
 		{ }
 
 		bool open()
 		{
-			return m_datafile.open();
+			return m_datafilemanager.open();
 		}
 
 		bool open(const std::string& path)
 		{
-			return m_datafile.open(path);
+			return m_datafilemanager.open(path);
 		}
 	};
 }
