9ea3fe5018d2ea7e9679a9b422f8294cb4a4ca1c
Christoph Malek <malekchristoph@gmail.com>
2014-05-09 20:48:35 +0200

    wip: map_datafile_items

--- include/twl/files/map/map_datafile_items.hpp
+++ include/twl/files/map/map_datafile_items.hpp
@@ -3,9 +3,11 @@
 // See LICENSE for more information.
 //
 
-#ifndef TWL_FILES_MAP_DATAFILE_ITEMS_H
-#define TWL_FILES_MAP_DATAFILE_ITEMS_H
+#ifndef TWL_FILES_MAP_MAP_DATAFILE_ITEMS_H
+#define TWL_FILES_MAP_MAP_DATAFILE_ITEMS_H
 
+
+#include <mlk/types/types.h>
 
 #include <cstring>
 
@@ -16,30 +18,148 @@ namespace twl
 	{
 		struct map_datafile_header
 		{
-			char m_sig[4];
-			int m_ver;
-			int m_size;
-			int m_swaplen;
-			int m_num_itemtypes;
-			int m_num_items;
-			int m_num_rawdata;
-			int m_item_area_size;
-			int m_data_area_size;
+			char sig[4];
+			int ver;
+			int size;
+			int swaplen;
+			int num_itemtypes;
+			int num_items;
+			int num_rawdata;
+			int item_area_size;
+			int data_area_size;
 
 			bool valid() const noexcept
-			{return ((std::memcmp(m_sig, "DATA", 4) == 0) && (m_ver == 4));}
+			{return ((std::memcmp(sig, "DATA", 4) == 0) && (ver == 4));}
 
 			int body_size() const noexcept
 			{
-				return (m_num_itemtypes * 12) +
-						(m_num_items * sizeof(int)) +
-						((m_num_rawdata * sizeof(int)) * 2) +
-						m_item_area_size + m_data_area_size;
+				return (num_itemtypes * 12) +
+						(num_items * sizeof(int)) +
+						((num_rawdata * sizeof(int)) * 2) +
+						item_area_size + data_area_size;
 			}
+		};
+
+		struct map_datafile_iteminfo
+		{
+			int type;
+			int start;
+			int num;
+		};
+
+		struct map_datafile_item
+		{
+			int type;
+			int id;
+			mlk::data_packet data;
+		};
+
+
+		//
+
+		struct color
+		{
+			int r, g, b, a;
+		};
+
+		struct basic_map_datafile_item
+		{
+			int type_and_id;
+			int length;
+		};
+
+		struct map_datafile_version
+		{
+			int version;
+		};
+
+		struct map_datafile_info
+		{
+			int version;
+			int author;
+			int map_version;
+			int credits;
+			int license;
+		};
+
+		struct map_datafile_image
+		{
+			int version;
+			int width;
+			int height;
+			int external;
+			int image_name;
+			int image_data;
+		};
+
+		struct map_datafile_group
+		{
+			int version;
+			int offset_x;
+			int offset_y;
+			int parallax_x;
+			int parallax_y;
+			int startlayer;
+			int numlayers;
+			int use_clipping;
+			int clip_x;
+			int clip_y;
+			int clip_w;
+			int clip_h;
+			int name[3];
+		};
+
+		struct map_datafile_layer
+		{
+			int version;
+			int type;
+			int flags;
+		};
+
+		struct map_datafile_layer_tilemap
+		{
+			map_datafile_layer layer;
+			int version;
+			int width;
+			int height;
+			int flags;
+			color col;
+			int colorenv;
+			int colorenv_offset;
+			int image;
+			int data;
+			int name[3];
+		};
+
+		struct map_datafile_layer_quads
+		{
+			map_datafile_layer layer;
+			int version;
+			int num_quads;
+			int data;
+			int image;
+			int name[3];
+		};
+
+		struct map_datafile_envpoint
+		{
+			int time;
+			int curevetype;
+			int values[4];
+		};
+
+		struct map_datafile_envelope
+		{
+			int version;
+			int channels;
+			int startpoint;
+			int numpoints;
+			int name[3];
+			int syncron;
 		};
 	}
 }
 
 
-#endif // TWL_FILES_MAP_DATAFILE_ITEMS_H
+#endif // TWL_FILES_MAP_MAP_DATAFILE_ITEMS_H
 
