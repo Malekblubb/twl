e41177917f90df7ae8bee7d996310ac7bb8f290d
Christoph Malek <malekchristoph@gmail.com>
2014-05-10 20:58:58 +0200

    added basic_map_layer

---
+++ include/twl/files/map/basic_map_layer.hpp
@@ -0,0 +1,33 @@
+//
+// Copyright (c) 2013-2014 Christoph Malek
+// See LICENSE for more information.
+//
+
+#ifndef TWL_FILES_MAP_BASIC_MAP_LAYER_H
+#define TWL_FILES_MAP_BASIC_MAP_LAYER_H
+
+
+#include "map_constants.hpp"
+
+
+namespace twl
+{
+	namespace internal
+	{
+		class basic_map_layer
+		{
+			map_constants::layer_type m_type;
+
+		public:
+			basic_map_layer(map_constants::layer_type type) :
+				m_type{type}
+			{ }
+
+			auto type() const noexcept
+			{return m_type;}
+		};
+	}
+}
+
+
+#endif // TWL_FILES_MAP_BASIC_MAP_LAYER_H
