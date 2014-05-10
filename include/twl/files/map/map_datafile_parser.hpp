3d5265f01ef13b1d4a3dc16cae6f5cc996d869d4
Christoph Malek <malekchristoph@gmail.com>
2014-05-10 11:47:54 +0200

    files > map > parser: cleaned up uncompressing data directly

--- include/twl/files/map/map_datafile_parser.hpp
+++ include/twl/files/map/map_datafile_parser.hpp
@@ -9,9 +9,8 @@
 
 #include "map_datafile.hpp"
 
-#include <mlk/containers/container_utl.h>
-#include <mlk/graphics/image.h>
 #include <mlk/compression/compression.h>
+#include <mlk/containers/container_utl.h>
 
 
 namespace twl
@@ -33,6 +32,9 @@ namespace twl
 				this->parse();
 			}
 
+			const map_datafile& result() const noexcept
+			{return m_result;}
+
 		private:
 			void parse()
 			{
@@ -41,19 +43,6 @@ namespace twl
 
 				// cast header
 				auto* header(reinterpret_cast<const map_datafile_header*>(m_raw->data()));
-
-				// init the result obj
-//				m_result.init(*header);
-
-				// parse
-//				std::map<int, map_datafile_iteminfo> infos;
-
-//				for(auto i(0); i < header->num_itemtypes; ++i)
-//				{
-//					mlk::data_packet tmp{std::begin(*m_raw) + sizeof(map_datafile_header) + i * sizeof(map_datafile_iteminfo), std::end(*m_raw)};
-//					auto* iteminfo(reinterpret_cast<const map_datafile_iteminfo*>(tmp.data()));
-////					infos.emplace(iteminfo->, *iteminfo);
-//				}
 
 				// skip item types & item offsets
 				mlk::st pos{sizeof(map_datafile_header) + header->num_itemtypes * sizeof(map_datafile_iteminfo) + header->num_items * 4};
@@ -71,7 +60,6 @@ namespace twl
 				pos += 4 * header->num_rawdata;
 
 
-
 				// parse the items
 				for(auto i(0); i < header->num_items; ++i)
 				{
@@ -87,36 +75,33 @@ namespace twl
 					// move forward
 					pos += sizeof(basic_map_datafile_item) + iteminfo->length;
 				}
-//				std::cout << m_result.num_items_of_type<map_constants::item_type::group>() << std::endl;
-//				std::cout << m_result.item<map_constants::item_type::group>(0)->offset_x << std::endl;
 
 
-				std::vector<mlk::data_packet> data(header->num_rawdata);
+				// parse data, decompress it directly
+				std::vector<mlk::data_packet> uncompressed_data(header->num_rawdata);
 				for(auto i(0); i < header->num_rawdata; ++i)
 				{
 					if(i != header->num_rawdata - 1)
 					{
-						data[i] = mlk::data_packet{std::begin(*m_raw) + pos, std::begin(*m_raw) + pos + (data_offsets[i + 1] - data_offsets[i])};
+						mlk::cmprs::compressor_zlib cmprs{{std::begin(*m_raw) + pos, std::begin(*m_raw) + pos + (data_offsets[i + 1] - data_offsets[i])}};
+						cmprs.unpack(uncompressed_sizes[i]);
+						if(cmprs.is_valid())
+							uncompressed_data[i] = cmprs.get();
+
 						pos += data_offsets[i + 1] - data_offsets[i];
 						continue;
 					}
 
-					data[i] = mlk::data_packet{std::begin(*m_raw) + pos, std::begin(*m_raw) + pos + header->data_area_size - data_offsets[i]};
+					mlk::cmprs::compressor_zlib cmprs{{std::begin(*m_raw) + pos, std::begin(*m_raw) + pos + header->data_area_size - data_offsets[i]}};
+					cmprs.unpack(uncompressed_sizes[i]);
+					if(cmprs.is_valid())
+						uncompressed_data[i] = cmprs.get();
+
 					pos += header->data_area_size - data_offsets[i];
 				}
 
-				for(auto& a : m_result.items_of_type<map_constants::item_type::image>())
-				  std::cout << a->image_data << std::endl;
-				std::cout << data[1].size() << std::endl;
-				std::cout << uncompressed_sizes[1] << std::endl;
-
-				mlk::cmprs::compressor_zlib cmprs{data[1]};
-				cmprs.unpack(uncompressed_sizes[1]);
-
-
-				mlk::gcs::image img{cmprs.get(), 1024, 512};
-				img.save("/home/hax/Desktop/test.png");
-
+				// move the data to result
+				m_result.add_uncompressed_data(uncompressed_data);
 			}
 		};
 	}
