44b38c19a792751300f7c01923c320671c9cdf30
Christoph Malek <malekchristoph@gmail.com>
2014-05-11 15:45:23 +0200

    files > map > map_layer_quads: implemented name implemented quads

--- include/twl/files/map/map_layer_quads.hpp
+++ include/twl/files/map/map_layer_quads.hpp
@@ -21,14 +21,23 @@ namespace twl
 		friend class map;
 
 		const map_image* m_image{nullptr};
+		std::vector<internal::quad> m_quads;
 		std::string m_name;
-
-		// std::vector<quad>
 
 		map_layer_quads(const internal::map_datafile_layer_quads* qds, const std::vector<mlk::data_packet>& uncompressed_data, const std::vector<map_image>& images) :
 			internal::basic_map_layer{map_constants::layer_type::quads},
-			m_image{qds->image >= 0 ? &images.at(qds->image) : nullptr}
-		{ }
+			m_image{qds->image >= 0 ? &images.at(qds->image) : nullptr},
+			m_quads(qds->num_quads),
+			m_name{map_constants::ints_to_str(qds->name, 3)}
+		{
+			// add the quads
+			mlk::data_packet tmp(sizeof(internal::quad));
+			for(auto i(0); i < uncompressed_data[qds->data].size(); i += sizeof(internal::quad))
+			{
+				tmp = {std::begin(uncompressed_data[qds->data]) + i, std::end(uncompressed_data[qds->data])};
+				m_quads[i / sizeof(internal::quad)] = *reinterpret_cast<const internal::quad*>(tmp.data());
+			}
+		}
 
 	public:
 		map_layer_quads() = default;
@@ -38,6 +47,12 @@ namespace twl
 
 		auto has_image() const noexcept
 		{return m_image != nullptr;}
+
+		const auto& name() const noexcept
+		{return m_name;}
+
+		const auto& quads() const noexcept
+		{return m_quads;}
 	};
 }
 
