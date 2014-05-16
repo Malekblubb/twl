4e264d5c80e447a535732fbafe3082d31a339bef
Christoph Malek <malekchristoph@gmail.com>
2014-05-16 21:15:06 +0200

    files > map > basic_map_layer: type is 'invalid' if no type given

--- include/twl/files/map/basic_map_layer.hpp
+++ include/twl/files/map/basic_map_layer.hpp
@@ -19,7 +19,7 @@ namespace twl
 			map_constants::layer_type m_type;
 
 		public:
-			basic_map_layer(map_constants::layer_type type) :
+			basic_map_layer(map_constants::layer_type type = map_constants::layer_type::invalid) :
 				m_type{type}
 			{ }
 
